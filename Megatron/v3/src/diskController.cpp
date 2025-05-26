5326adefa313f1333ea8792d13c58446b6e0d920
CHOCEK <choceksalas@gmail.com>
2025-05-26 16:02:17 -0500

    Nothing

--- Megatron/v3/src/diskController.cpp
+++ Megatron/v3/src/diskController.cpp
@@ -3,112 +3,128 @@
 #include <head.hpp>
 #include <unistd.h>
 
-DiskController::DiskController(int numberDisks, int numberTracks, int numberSectors, int numberBytes, int sectorsBlock)
+DiskController::DiskController(int numberDisks,
+                               int numberTracks,
+                               int numberSectors,
+                               int numberBytes,
+                               int sectorsBlock)
     : numberDisks(numberDisks), numberTracks(numberTracks),
-      numberSectors(numberSectors), numberBytes(numberBytes), sectorsBlock(sectorsBlock) {
+      numberSectors(numberSectors), numberBytes(numberBytes),
+      sectorsBlock(sectorsBlock) {
   head = new Head;
 };
 
 DiskController::~DiskController() { delete head; }
 
 void DiskController::moveHeadTo(int disk, int surface, int track, int sector) {
-  if (head ->currentFd != -1){
+  if (head->currentFd != -1) {
     close(head->currentFd);
     head->currentFd = -1;
   }
-  
-  head->moveTo(disk%numberDisks, surface%2, track%numberTracks, sector%numberSectors);
+
+  head->moveTo(disk % numberDisks,
+               surface % 2,
+               track % numberTracks,
+               sector % numberSectors);
 }
 
 void DiskController::nextSector() {
-    head->currentSector++;
+  head->currentSector++;
 
-    if (head->currentSector >= numberSectors) {
-        head->currentSector = 0;
-        nextTrack();
-    }
+  if (head->currentSector >= numberSectors) {
+    head->currentSector = 0;
+    nextTrack();
+  }
 }
 
-
 void DiskController::nextTrack() {
-    head->currentTrack++;
+  head->currentTrack++;
 
-    if (head->currentTrack >= numberTracks) {
-        head->currentTrack = 0;
-        nextSurface();
-    }
+  if (head->currentTrack >= numberTracks) {
+    head->currentTrack = 0;
+    nextSurface();
+  }
 }
 
 void DiskController::nextSurface() {
-    head->currentSurface++;
+  head->currentSurface++;
 
-    if (head->currentSurface >= 2) {
-        head->currentSurface = 0;
-        nextDisk();
-    }
+  if (head->currentSurface >= 2) {
+    head->currentSurface = 0;
+    nextDisk();
+  }
 }
 
 void DiskController::nextDisk() {
-    head->currentDisk++;
+  head->currentDisk++;
 
-    if (head->currentDisk >= numberDisks) {
-        head->currentDisk = 0;
-    }
+  if (head->currentDisk >= numberDisks) {
+    head->currentDisk = 0;
+  }
 }
 
 void DiskController::moveToBlock(uint32_t blockID) {
-    int blocksPerTrack = numberSectors / sectorsBlock;
-    int blocksPerSurface = numberTracks * blocksPerTrack;
-    int blocksPerDisk = 2 * blocksPerSurface;
+  int blocksPerTrack = numberSectors / sectorsBlock;
+  int blocksPerSurface = numberTracks * blocksPerTrack;
+  int blocksPerDisk = 2 * blocksPerSurface;
 
-    int disk = blockID / blocksPerDisk;
-    blockID %= blocksPerDisk;
+  int disk = blockID / blocksPerDisk;
+  blockID %= blocksPerDisk;
 
-    int surface = blockID / blocksPerSurface;
-    blockID %= blocksPerSurface;
+  int surface = blockID / blocksPerSurface;
+  blockID %= blocksPerSurface;
 
-    int track = blockID / blocksPerTrack;
-    int blockInTrack = blockID % blocksPerTrack;
+  int track = blockID / blocksPerTrack;
+  int blockInTrack = blockID % blocksPerTrack;
 
-    int sector = blockInTrack * sectorsBlock;
+  int sector = blockInTrack * sectorsBlock;
 
-    head->moveTo(disk, surface, track, sector);
-    head->openCurrentSectorFD();
+  head->moveTo(disk, surface, track, sector);
+  head->openCurrentSectorFD();
 }
 
-
 void DiskController::initializeFAT() {
-    moveToBlock(1);
+  moveToBlock(1);
 
-    uint32_t totalBlocks =
-        numberDisks * 2 * numberTracks * (numberSectors / sectorsBlock);
+  uint32_t blockSize = numberBytes * sectorsBlock;
+  uint32_t entriesPerBlock = blockSize / sizeof(uint32_t);
 
-    // 4 bytes en 0 para bloques libres
-    uint32_t empty = 0; 
+  uint32_t totalBlocks =
+      numberDisks * 2 * numberTracks * (numberSectors / sectorsBlock);
 
-    for (uint32_t i = 0; i < totalBlocks; ++i) {
-        writeBinary(empty);
+  uint32_t fatBlocks = (totalBlocks + entriesPerBlock - 1) / entriesPerBlock;
+
+  // 4 bytes en 0 para bloques libres
+  uint32_t empty = 0;
+  uint32_t written = 0;
+
+  for (uint32_t b = 0; b < fatBlocks; ++b) {
+    moveToBlock(1 + b);
+    for (uint32_t i = 0; i < entriesPerBlock && written < totalBlocks; ++i) {
+      writeBinary(empty);
+      ++written;
     }
+  }
 
-    head->resetPosition();
+  head->resetPosition();
 }
 
 void DiskController::writeFATEntry(uint32_t blockID, uint32_t nextBlockID) {
-    moveToBlock(1);
+  moveToBlock(1);
 
-    uint32_t offset = blockID * sizeof(uint32_t);
-    lseek(head->currentFd, offset, SEEK_CUR);
+  uint32_t offset = blockID * sizeof(uint32_t);
+  lseek(head->currentFd, offset, SEEK_CUR);
 
-    writeBinary(nextBlockID);
+  writeBinary(nextBlockID);
 }
 
 uint32_t DiskController::readFATEntry(uint32_t blockID) {
-    moveToBlock(1);
+  moveToBlock(1);
 
-    uint32_t offset = blockID * sizeof(uint32_t);
-    lseek(head->currentFd, offset, SEEK_CUR);
+  uint32_t offset = blockID * sizeof(uint32_t);
+  lseek(head->currentFd, offset, SEEK_CUR);
 
-    uint32_t nextBlock;
-    readBinary(nextBlock);
-    return nextBlock;
+  uint32_t nextBlock;
+  readBinary(nextBlock);
+  return nextBlock;
 }
